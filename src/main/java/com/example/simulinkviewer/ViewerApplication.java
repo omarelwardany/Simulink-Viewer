83d68379d980ee20db2301a0ff550e7833101ccf
Omar Emad <omarelwardany@outlook.com>
2023-05-17 13:42:54 +0300

    Renamed File

---
+++ src/main/java/com/example/simulinkviewer/ViewerApplication.java
@@ -0,0 +1,16 @@
+package com.example.simulinkviewer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class ViewerApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.simulinkviewer {
     requires javafx.controls;
     requires javafx.fxml;
+    requires engine;
+    requires java.xml;
 
 
     opens com.example.simulinkviewer to javafx.fxml;
