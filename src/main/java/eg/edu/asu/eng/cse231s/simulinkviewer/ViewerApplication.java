a5cc8bcc568cae3edbdac59259132a4004fc5c04
Omar Emad <omarelwardany@outlook.com>
2023-05-18 01:00:03 +0300

    Project Initial Structure

--- src/main/java/eg/edu/asu/eng/cse231s/simulinkviewer/ViewerApplication.java
+++ src/main/java/eg/edu/asu/eng/cse231s/simulinkviewer/ViewerApplication.java
@@ -2,12 +2,13 @@ package eg.edu.asu.eng.cse231s.simulinkviewer;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.w3c.dom.Document;
+
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ViewerApplication extends Application {
@@ -17,7 +18,23 @@ public class ViewerApplication extends Application {
         *   Textbox and button to get file path, then save it to a String called mdlPath*/
         String mdlPath = /* Test path */ "C:\\Users\\OmarEmadSayedEl-Ward\\Desktop\\Example.mdl";
 
-        File newXML = extractXML(mdlPath);
+        // converts the mdl file to an ArrayList of blocks and an ArrayList for lines to be drawn
+        File XMLFile = extractXML(mdlPath);
+        NodeList blockNodes = blocksFromXML(XMLFile);
+        NodeList lineNodes = linesFromXML(XMLFile);
+        List<Element> blocks = new ArrayList<>();
+        List<Element> lines = new ArrayList<>();
+        for (int i = 0; i < blockNodes.getLength(); i++) {
+            blocks.add((Element) blockNodes.item(i));
+        }
+        for (int i = 0; i < lineNodes.getLength(); i++) {
+            lines.add((Element) lineNodes.item(i));
+        }
+
+        /* TODO: Scene
+        *   for each block, get dimensions and position, then add to scene
+        *   for each line, get source point and destination point, then add to scene*/
+
     }
 
     public static void main(String[] args) { launch(); }
@@ -28,8 +45,14 @@ public class ViewerApplication extends Application {
         // TODO: Issue #1
     }
 
-    /* Reads XML file and extracts all of its elements into a NodeList */
-    public static NodeList parseXML(String XMLPath) {
+    /* Reads XML file and extracts all of its Block elements into a NodeList */
+    public static NodeList blocksFromXML(File XMLFile) {
         // TODO: Issue #3
     }
+
+    /* Reads XML file and extracts all of its Line elements into a NodeList */
+    public static NodeList linesFromXML(File XMLFile) {
+        // TODO: Issue #4
+    }
+
 }
