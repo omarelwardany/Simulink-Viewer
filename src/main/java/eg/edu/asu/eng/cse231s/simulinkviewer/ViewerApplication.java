a048a8dcb548f2d1417ccffab7c5c61f51d63606
OmarT-Y <omartamerz9@gmail.com>
2023-05-20 03:02:18 +0300

    Improved Project structure-Added a new class to contain some methods

--- src/main/java/eg/edu/asu/eng/cse231s/simulinkviewer/ViewerApplication.java
+++ src/main/java/eg/edu/asu/eng/cse231s/simulinkviewer/ViewerApplication.java
@@ -26,6 +26,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
+import static eg.edu.asu.eng.cse231s.simulinkviewer.PosHandler.*;
 
 
 public class ViewerApplication extends Application {
@@ -262,45 +263,6 @@ public class ViewerApplication extends Application {
         return xmlDoc.getElementsByTagName(tagName);
     }
 
-
-
-    /* Takes String like "[1040, 283, 1075, 317]" and returns X value of the rectangle as a double*/
-    public static double getXFromPosition(String position) {
-        String[] positionTag =  position.replaceAll("\\[" ,"" ).replaceAll("]" , "").split(",");
-
-        Double x = Double.parseDouble(positionTag[0]);
-
-        return x;
-    }
-
-    /* Takes String like "[1040, 283, 1075, 317]" and returns Y value of the rectangle as a double*/
-    public static double getYFromPosition(String position) {
-        String[] positionTag =  position.replaceAll("\\[" ,"" ).replaceAll("]" , "").split(",");
-
-        Double y = Double.parseDouble(positionTag[1]);
-
-        return y;
-    }
-
-    /* Takes String like "[1040, 283, 1075, 317]" and returns Width value of the rectangle as a double*/
-    public static double getWidthFromPosition(String position) {
-        String[] positionTag =  position.replaceAll("\\[" ,"" ).replaceAll("]" , "").split(", ");
-
-        Double widthPlusX = Double.parseDouble(positionTag[2]);
-        Double x = Double.parseDouble(positionTag[0]);
-        Double width = widthPlusX - x;
-        return width;
-    }
-
-    /* Takes String like "[1040, 283, 1075, 317]" and returns Height value of the rectangle as a double*/
-    public static double getHeightFromPosition(String position) {
-        String[] positionTag =  position.replaceAll("\\[" ,"" ).replaceAll("]" , "").split(", ");
-
-        Double heightPlusY = Double.parseDouble(positionTag[3]);
-        Double y = Double.parseDouble(positionTag[1]);
-        Double height = heightPlusY - y;
-        return height;
-    }
 
     public static Point[] getPointsFromString(String pointsString) {
         Point[] points;
